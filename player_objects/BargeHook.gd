5d56aba6ea54b3cdc30ae045376455d2e45ec05c
Kiermasz <raczynski90@gmail.com>
2021-05-08 15:19:32 +0200

    chain disengages on strain

--- player_objects/BargeHook.gd
+++ player_objects/BargeHook.gd
@@ -4,10 +4,12 @@ onready var barge_find_area = get_node("barge_locator")
 onready var chain_node = get_parent()
 onready var locked : bool = false
 onready var locked_timer : float = 5
+onready var object_attached = null
+onready var player = Global.Player
+onready var distance_to_attached : float = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,16 +19,30 @@ func _process(delta):
 
 	if not locked and locked_timer > 0:
 		locked_timer -= delta
+	
+	if locked:
+		# check for strain on the link
+		# if max distance exceeded, disengage lock
+		distance_to_attached = player.translation.distance_to(object_attached.translation)
+		if distance_to_attached > 20:
+			unlock()
+
+
 
 func _on_barge_locator_body_entered(body):
+	# detect if chain hook collided with pullable object
+	# if collision is detected attach object to chain
 	if (not locked) and (locked_timer <= 0):
 		if body.is_in_group("barges"):
 			self.set("nodes/node_a", body.get_path())
 			self.set("nodes/node_b", chain_node.get_path())
 			locked = true
+			object_attached = body
 
 func unlock():
+	# release the currently pulled object from chain
 	locked = false
 	locked_timer = 5
 	self.set("nodes/node_a", "")
 	self.set("nodes/node_b", "")
+	object_attached = null

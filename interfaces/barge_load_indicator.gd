55a461a511a035913a08c869c43e5f904ad20287
Kiermasz <raczynski90@gmail.com>
2021-05-26 21:46:14 +0200

    added tutorial level 2, tutorial button in main menu

--- interfaces/barge_load_indicator.gd
+++ interfaces/barge_load_indicator.gd
@@ -1,3 +1,7 @@
+# collection of 3d objects displayed in relation to main camera
+# contains UI for the number of cargo loaded on all barges
+# updated from LevelTemplate node
+
 extends Spatial
 
 onready var crate_icon = load("res://interfaces/gauges/cargo_gauge.tscn")
@@ -6,10 +10,15 @@ onready var for_deletion = null
 onready var for_deletion_queue = []
 
 func increase_bar():
+	# entry point, add one crate icon
 	spawn_icon(horizontal_offset, 0, 0)
 	horizontal_offset += 0.6
 
 func decrease_bar():
+	# entry point, remove one crate icon
+	# adds last created crate to deletion queue
+	# deletion queue is later handled in physics process
+	# to allow for the entire animation to play out
 	var i = 1
 	for_deletion = get_children()[-i]
 	while for_deletion in for_deletion_queue:
@@ -17,8 +26,6 @@ func decrease_bar():
 		for_deletion = get_children()[-i]
 	for_deletion_queue.append(for_deletion)
 		
-	
-
 func spawn_icon(x,y,z):
 	add_child(crate_icon.instance())
 	var new_crate_icon = get_node(get_children()[-1].get_path())
@@ -33,4 +40,3 @@ func _physics_process(_delta):
 				horizontal_offset -= 0.6
 				for_deletion_queue.erase(obj)
 				obj.queue_free()
-				print("obj deleted")

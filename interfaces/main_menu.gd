b00a2cefaf41e73d7a2f071c36165733279a3a65
Kiermasz <raczynski90@gmail.com>
2021-07-10 14:10:26 +0200

    ice level basic layout

--- interfaces/main_menu.gd
+++ interfaces/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_NewGame_button_up():
-	pass
+	return get_tree().change_scene("res://levels/level1.tscn")
 
 func _on_Tutorial_button_up():
 	return get_tree().change_scene("res://levels/1Tutorial.tscn")

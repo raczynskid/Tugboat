578359fa67138a61f76123658dd4cb2285d72d01
Kiermasz <raczynski90@gmail.com>
2021-07-30 22:41:15 +0200

    custom messages for welcome screen based on level

--- interfaces/welcome_level_screen.gd
+++ interfaces/welcome_level_screen.gd
@@ -3,14 +3,23 @@ extends Node
 onready var ship_img = get_node("Ship_message/Ship_pic")
 onready var msg = get_node("Ship_message/msg")
 onready var bar = get_node("bar")
+onready var text = get_node("Ship_message/msg/RichTextLabel")
 onready var gone : bool = false
+
+
+func _ready():
+	var level_name = get_tree().get_current_scene().name
+	var welcome_messages = {"Level1" : "deliver 2 cargo to pass",
+						"Tutorial" : "Movement tutorial",
+						"EnvironmentTutorial" : "deliver 2 cargo to pass"}
+	text.text = welcome_messages[level_name]
 
 func move():
 	ship_img.position.x += 10
-	msg.position.x += 8
+	msg.position.x += 7
 
 func disappear():
-	bar.modulate.a -= 0.03
+	bar.modulate.a -= 0.005
 
 func _physics_process(delta):
 	if ship_img.position.x < 1200:
---
+++ levels/level1.gd
@@ -0,0 +1,4 @@
+extends Spatial
+
+onready var welcome_text = "2 cargo to pass"
+
